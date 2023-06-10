3e7729d3cc8f7a314f9f05ea9d26fc50a59335c5
kypkk <kypss50102@gmail.com>
2023-06-10 10:24:38 +0800

    feat: about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,8 +3,11 @@ import "../Styles/Home.css";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import CallIcon from "@mui/icons-material/Call";
+import Image1 from "../Images/凃建名.jpg";
 import Image2 from "../Images/彭康彧.jpg";
-import Image4 from "../Images/image4.jpg";
+import Image3 from "../Images/雪花冰.jpg";
+import Image4 from "../Images/楊晴方.jpg";
+import Image5 from "../Images/陳品蓁.jpg";
 
 const About = () => {
   return (
@@ -13,14 +16,13 @@ const About = () => {
       <div className="members flex align-center justify-center flex-col lg:flex-row">
         <section className="flex align-center justify-center flex-col md:flex-row ">
           <div className="member my-5 mx-12 md:m-5">
-            <img width={200} height={200} src={Image4} className="my-4" />
+            <img width={200} height={200} src={Image1} className="my-4" />
             <div className="description">
               <h1 className=" text-3xl my-4">凃建名</h1>
               <h2 className=" text-lg my-4">組員</h2>
-              <p>簡介</p>
-              <p>簡介</p>
-              <p>簡介</p>
-              <p className=" mb-4 ">簡介</p>
+              <p>凃建名</p>
+              <p>中央資工碩一</p>
+              <p className=" mb-4 ">資工小廢人</p>
               <div className="social-media">
                 <InstagramIcon />
                 <FacebookIcon />
@@ -35,7 +37,6 @@ const About = () => {
               <h2 className=" text-lg my-4">組員</h2>
               <p>彭康彧 aka 康康</p>
               <p>中央大學數學系 ➡️ 資工系</p>
-              <p>吉巴神🔥</p>
               <p className=" mb-4 ">資料庫ㄌㄢ波萬☝🏼</p>
               <div className="social-media">
                 <InstagramIcon />
@@ -47,14 +48,13 @@ const About = () => {
         </section>
         <section className="flex align-center justify-center flex-col lg:flex-row">
           <div className="member my-5 mx-12 md:m-5">
-            <img width={200} height={200} src={Image4} className="my-4" />
+            <img width={200} height={200} src={Image3} className="my-4" />
             <div className="description">
               <h1 className=" text-3xl my-4">蔡明翰</h1>
               <h2 className=" text-lg my-4">組員</h2>
               <p>蔡明翰 aka 雪花冰</p>
               <p>中央大學資工系</p>
-              <p>簡介</p>
-              <p className=" mb-4 ">簡介</p>
+              <p className=" mb-4 ">ppt 專家</p>
               <div className="social-media">
                 <InstagramIcon />
                 <FacebookIcon />
@@ -68,9 +68,8 @@ const About = () => {
               <h1 className=" text-3xl my-4">楊晴方</h1>
               <h2 className=" text-lg my-4">組員</h2>
               <p>楊晴方</p>
-              <p>簡介</p>
-              <p>簡介</p>
-              <p className=" mb-4 ">簡介</p>
+              <p>中央大學資工系</p>
+              <p className=" mb-4 ">大3B班</p>
               <div className="social-media">
                 <InstagramIcon />
                 <FacebookIcon />
@@ -79,14 +78,13 @@ const About = () => {
             </div>
           </div>
           <div className="member my-5 mx-12 md:m-5">
-            <img width={200} height={200} src={Image4} className="my-4" />
+            <img width={200} height={200} src={Image5} className="my-4" />
             <div className="description">
               <h1 className=" text-3xl my-4">陳品蓁</h1>
               <h2 className=" text-lg my-4">組員</h2>
               <p>陳品蓁</p>
-              <p>簡介</p>
-              <p>簡介</p>
-              <p className=" mb-4 ">簡介</p>
+              <p>中央碩士就讀中</p>
+              <p className=" mb-4 ">美食觀察家👀</p>
               <div className="social-media">
                 <InstagramIcon />
                 <FacebookIcon />
